e3d8cb1b0220426414ba64735962299c97a11f31
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-04-06 19:12:19 -0700

    compressed my html

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+# a script to compress html file to tar
 from fabric.api import local
 from datetime import datetime
 def do_pack():
+    """a function to compres the file"""
 	time_now = datetime.now().strftime("%Y%m%d%H%M%S")
 	archive_filepath = "versions/web_static_{}.tgz".format(time_now)
 	try:

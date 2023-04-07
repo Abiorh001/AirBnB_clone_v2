f4b84c364b0c48d58d5daf6c4b3488f657905757
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-04-06 19:19:08 -0700

    compressed my html

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-# a script to compress html file to tar
+""" a script to compress html file to tar"""
 from fabric.api import local
 from datetime import datetime
+
+
 def do_pack():
     """a function to compres the file"""
-	time_now = datetime.now().strftime("%Y%m%d%H%M%S")
-	archive_filepath = "versions/web_static_{}.tgz".format(time_now)
-	try:
-		local("mkdir -p versions")
-		local("tar -czvf {} web_static".format(archive_filepath))
-		return "{}".format(archive_filepath)
-	except Exception as e:
-		return None
+    time_now = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_filepath = "versions/web_static_{}.tgz".format(time_now)
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf {} web_static".format(archive_filepath))
+        return "{}".format(archive_filepath)
+    except Exception as e:
+        return None

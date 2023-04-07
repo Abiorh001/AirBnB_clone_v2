52a126c7c9febb06a9d56f5afeaadc0279c7af31
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-04-06 18:54:55 -0700

    deploy

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
-# script to install and configure nginx
+#a script to install nginx and create a dummy index.html
 
-#install nginx
+#updating the server
 sudo apt-get update
+# installing nginxwebserver
 sudo apt-get install nginx -y
 
-#creating directories
+#creating the directory to store my html file
 sudo mkdir -p /data/web_static/releases/test/
+#creating the directory for the symbolic link
 sudo mkdir -p /data/web_static/shared/
 
-#creating a html page
+#creating a dummy index.html file to write inside my test directory
 echo "<!DOCTYPE html>
 <html>
   <head>
@@ -19,14 +21,11 @@ echo "<!DOCTYPE html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-#creating a symbolic link
+#creating a symbolic link t0 link our index.html to current directory
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-#changing directory name
+# changing ownerning of the file to user ubutn and group ubuntu
 sudo chown -R ubuntu:ubuntu /data/
-
-#setting up the page to be served
+#creating the location on the sever to served the index.html
 sudo sed -i '/server_name _;/a \ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
-
-#restart the server
+#restart the nginx webserver
 sudo service nginx restart

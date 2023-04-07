99827a3b33ae1c22074524c0799799afeda7e8c4
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-04-07 03:43:18 -0700

    deploy to servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python3
-"""a script to add compressed file to server
-   the script will uncompreesed the files
-   the script will served the servers the index.html files
 """
-from fabric.api import run, put, env
+A script to add compressed file to server.
+The script will uncompress the files andserve the servers the index.html files.
+"""
 import os.path
+from fabric.api import run, put, env
+
 env.hosts = ["54.172.57.51", "34.232.78.100"]
-env.key_filename = '~/.ssh/school'
+env.key_filename = os.path.expanduser('~/.ssh/school')
 env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """function that will run the script"""
+    """Function that will run the script."""
     if not os.path.exists(archive_path):
         return False
     put(archive_path, "/tmp/")
-    archive file path
     compressed_filepath = "versions/web_static_20230406235730.tgz"
     compressed_filename = compressed_filepath.split("/")[1]
     compressed_file_without_ext = compressed_filename.split(".")[0]
-    folder_uncompressed_file = "/data/web_static/releases/{}".format(compressed_file_without_ext)
+    folder_uncompressed_file = "/data/web_static/releases/{}".format(
+        compressed_file_without_ext)
     run("sudo mkdir -p {}".format(folder_uncompressed_file))
-    run("sudo tar -xzf /tmp/{} -C {}".format(compressed_filename, folder_uncompressed_file))
+    run("sudo tar -xzf /tmp/{} -C {}".format(
+        compressed_filename, folder_uncompressed_file))
     run("sudo rm /tmp/{} ".format(compressed_filename))
-    run("sudo mv {}/web_static/* {}".format(folder_uncompressed_file, folder_uncompressed_file))
+    run("sudo mv {}/web_static/* {}".format(
+        folder_uncompressed_file, folder_uncompressed_file))
     run("sudo rm -rf {}/web_static".format(folder_uncompressed_file))
     run("sudo rm -rf /data/web_static/current")
-    run("sudo ln -s {} /data/web_static/current".format(folder_uncompressed_file))
+    run("sudo ln -s {} /data/web_static/current".format(
+        folder_uncompressed_file))
     return True
